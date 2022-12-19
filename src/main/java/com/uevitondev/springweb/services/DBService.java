e1eb8a96e9b410b594c223a4ef11f9e1bedfbce7
Ueviton Santos <uevitonsantos@outlook.com>
2022-12-19 14:22:05 -0300

    Criando profile de test

---
+++ src/main/java/com/uevitondev/springweb/services/DBService.java
@@ -0,0 +1,133 @@
+package com.uevitondev.springweb.services;
+
+import com.uevitondev.springweb.domain.*;
+import com.uevitondev.springweb.domain.enums.EstadoPagamento;
+import com.uevitondev.springweb.domain.enums.TipoCliente;
+import com.uevitondev.springweb.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    @Autowired
+    private EstadoRepository estadoRepository;
+    @Autowired
+    private CidadeRepository cidadeRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
+
+    public void instatiateTestDatabase() throws ParseException {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+        Categoria cat3 = new Categoria(null, "Computadores");
+        Categoria cat4 = new Categoria(null, "Cama Mesa e Banho");
+        Categoria cat5 = new Categoria(null, "Eletrônicos");
+        Categoria cat6 = new Categoria(null, "Jardinagem");
+        Categoria cat7 = new Categoria(null, "Perfumaria");
+
+
+        Produto prod1 = new Produto(null, "Computador", 2000.00);
+        Produto prod2 = new Produto(null, "Impressora", 800.00);
+        Produto prod3 = new Produto(null, "Mouse", 80.00);
+        Produto prod4 = new Produto(null, "Mesa", 300.00);
+        Produto prod5 = new Produto(null, "Toalha", 50.00);
+        Produto prod6 = new Produto(null, "Colcha", 200.00);
+        Produto prod7 = new Produto(null, "TV True Color", 1200.00);
+        Produto prod8 = new Produto(null, "Roçadeira", 800.00);
+        Produto prod9 = new Produto(null, "Abajour", 100.00);
+        Produto prod10 = new Produto(null, "Pendente", 180.00);
+        Produto prod11 = new Produto(null, "Shampoo", 90.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+        cat2.getProdutos().addAll(Arrays.asList(prod2, prod4));
+        cat3.getProdutos().addAll(Arrays.asList(prod5, prod6));
+        cat4.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3, prod7));
+        cat5.getProdutos().addAll(Arrays.asList(prod8));
+        cat6.getProdutos().addAll(Arrays.asList(prod9, prod10));
+        cat7.getProdutos().addAll(Arrays.asList(prod11));
+
+
+        prod1.getCategorias().addAll(Arrays.asList(cat1));
+        prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        prod3.getCategorias().addAll(Arrays.asList(cat1));
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+        produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9, prod10, prod11));
+
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade cid1 = new Cidade(null, "Uberlandia", est1);
+        Cidade cid2 = new Cidade(null, "São Paulo", est2);
+        Cidade cid3 = new Cidade(null, "Campinas", est2);
+
+        est1.getCidades().addAll(Arrays.asList(cid1));
+        est2.getCidades().addAll(Arrays.asList(cid2, cid3));
+
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
+
+
+        Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "04011125987", TipoCliente.PESSOAFISICA);
+        cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+        Endereco end1 = new Endereco(null, "Rua das flores", "300", "Apto 203", "Jardim", "38220834", cli1, cid1);
+        Endereco end2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, cid2);
+
+        cli1.getEnderecos().addAll(Arrays.asList(end1, end2));
+
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        enderecoRepository.saveAll(Arrays.asList(end1, end2));
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
+        Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, end1);
+        Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, end2);
+
+
+        Pagamento pag1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+        ped1.setPagamento(pag1);
+
+        Pagamento pag2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+        ped2.setPagamento(pag2);
+
+        cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+        pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+        pagamentoRepository.saveAll(Arrays.asList(pag1, pag2));
+
+        ItemPedido ip1 = new ItemPedido(ped1, prod1, 0.00, 1, 2000.00);
+        ItemPedido ip2 = new ItemPedido(ped1, prod3, 0.00, 2, 80.00);
+        ItemPedido ip3 = new ItemPedido(ped2, prod2, 0.00, 1, 800.00);
+
+        ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+        ped2.getItens().addAll(Arrays.asList(ip3));
+
+        prod1.getItens().addAll(Arrays.asList(ip1));
+        prod2.getItens().addAll(Arrays.asList(ip3));
+        prod3.getItens().addAll(Arrays.asList(ip2));
+
+        itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+    }
+
+
+}
