12667e432fd6e08093d936262a3410a8dc035a7c
Ueviton Santos <uevitonsantos@outlook.com>
2022-12-19 15:30:22 -0300

    Criando profile dev

--- src/main/java/com/uevitondev/springweb/SpringWebApplication.java
+++ src/main/java/com/uevitondev/springweb/SpringWebApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringWebApplication implements CommandLineRunner {
 
-
     public static void main(String[] args) {
         SpringApplication.run(SpringWebApplication.class, args);
     }
