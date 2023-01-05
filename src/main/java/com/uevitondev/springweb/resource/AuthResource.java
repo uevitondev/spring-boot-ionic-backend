29a0d0d5e6532b087fb1fb2b8c0bcddd5d5ef38a
Ueviton Santos <uevitonsantos@outlook.com>
2023-01-05 16:37:51 -0300

    Refresh token

---
+++ src/main/java/com/uevitondev/springweb/resource/AuthResource.java
@@ -0,0 +1,28 @@
+package com.uevitondev.springweb.resource;
+
+import com.uevitondev.springweb.security.JWTUtil;
+import com.uevitondev.springweb.security.UserSS;
+import com.uevitondev.springweb.services.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @PostMapping(value = "/refresh_token")
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+}
